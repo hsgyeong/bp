27d7cdfe3554b3f261c30bdd09f03538fa179e8a
hsgyeong <sgyeong0711@gmail.com>
2026-06-01 14:30:18 +0900

    initial commit

---
+++ jaringochi/src/main/java/com/bp/jaringochi/JaringochiApplication.java
@@ -0,0 +1,13 @@
+package com.bp.jaringochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaringochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaringochiApplication.class, args);
+	}
+
+}
---
+++ jaringochi/src/main/java/com/bp/jaringochi/domain/user/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.bp.jaringochi.domain.user.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public Map<String, String> test() {
+        return Map.of("message", "백엔드 연결 성공");
+    }
+}
